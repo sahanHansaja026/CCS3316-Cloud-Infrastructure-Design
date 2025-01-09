0c252448ba156be7a6677b1f7237fa4565d274bc
Sahan Hansaja <165319843+sahanHansaja026@users.noreply.github.com>
2025-01-09 10:39:38 +0530

    bug 4

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,12 +1,8 @@
-import React from "react";
-import { act } from "@testing-library/react"; // Import act from 'react' instead of 'react-dom/test-utils'
 import { render, screen } from "@testing-library/react";
 import App from "./App";
-import "@testing-library/jest-dom"; // Add this import for toBeInTheDocument to work
 
-
-test("renders the project title", () => {
+test("renders learn react link", () => {
   render(<App />);
-  const projectTitle = screen.getByText(/CCS3316 Cloud Infrastructure Design/i);
-  expect(projectTitle).toBeInTheDocument();
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
